0ba388dc78d237acf6c0f8b7cf333b626001ac6c
Tatiana Saragossi <t.saragossi@students.ephec.be>
2023-05-11 17:41:29 +0200

    modif des tests

--- tests.py
+++ tests.py
@@ -30,15 +30,23 @@ class TestSimpleBin(unittest.TestCase):
 
     def test_get_snippet(self):
         """ Accessing a snippet that exist show the valid code """
-        with urlopen('/iexist') as res:
-            self.assertEqual(res.status, 200)
-            self.assertEqual(res.read().decode(), "Hello world")
+        import unittest.mock
+
+        snippet = simplebin.Snippet("iexist", "Hello world")
+        with unittest.mock.patch("simplebin.Snippet") as mock:
+            mock.get_by_name.return_value = snippet 
+            with urlopen('/iexist') as res:
+                self.assertEqual(res.status, 200)
+                self.assertEqual(res.read().decode(), "Hello world")
 
 
     def test_missing_snippet(self):
         """ Accessing a snippet that does not exist fails with a 404 """
-        with urlopen('/idontexist') as res:
-            self.assertEqual(res.status, 404)
+        import unittest.mock
+        with unittest.mock.patch("simplebin.Snippet") as mock:
+            mock.get_by_name.side_effect = ValueError() 
+            with urlopen('/idontexist') as res:
+                self.assertEqual(res.status, 404)
 
 
 if __name__ == '__main__':
