81d91920b1142c52c080061e41c13abd1a582025
Carlos Iglesias <92795126+iglesias1602@users.noreply.github.com>
2023-05-11 17:21:53 +0200

    Bugfix tests.py => Failing unit tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import urllib.request
 import unittest
+import unittest.mock
 from threading import Thread
 
 import simplebin
@@ -30,16 +31,23 @@ class TestSimpleBin(unittest.TestCase):
 
     def test_get_snippet(self):
         """ Accessing a snippet that exist show the valid code """
-        with urlopen('/iexist') as res:
-            self.assertEqual(res.status, 200)
-            self.assertEqual(res.read().decode(), "Hello world")
+        snippet = simplebin.Snippet("iexist", "Hello world")
+
+        with unittest.mock.patch('simplebin.Snippet') as mock_snippet:
+            mock_snippet.get_by_name.return_value = snippet
+            with urlopen('/iexist') as res:
+                self.assertEqual(res.status, 200)
+                self.assertEqual(res.read().decode(), "Hello world")
 
 
     def test_missing_snippet(self):
         """ Accessing a snippet that does not exist fails with a 404 """
-        with urlopen('/idontexist') as res:
-            self.assertEqual(res.status, 404)
 
+        with unittest.mock.patch('simplebin.Snippet') as mock_snippet:
+            mock_snippet.get_by_name.side_effect = ValueError("Value does not exist")
+            with urlopen('/idontexist') as res:
+                self.assertEqual(res.status, 404)
+           
 
 if __name__ == '__main__':
     unittest.main()
